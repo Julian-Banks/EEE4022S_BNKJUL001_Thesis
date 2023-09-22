81678b87ed7fed936c978d5763bf7400f38882a5
Julian-Banks <BNKJUL001@myuct.ac.za>
2023-09-22 17:57:39 +0200

    pythonTest

--- PythonWorkspace/test1.py
+++ PythonWorkspace/test1.py
@@ -1,1 +1,13 @@
-# load 
+# load data clean for me 
+#How do I add packages to the PATH
+
+import pandas as pd
+
+
+
+# 1.1 load data
+def load_data(path):
+    data = pd.read_csv(path)
+    return data
+#Load the csv file called dataCleaned.csv into a dataframe called data
+data = load_data('dataCleaned.csv')
